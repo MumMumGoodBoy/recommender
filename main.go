2e694a302f77459ead36d64bd9384569521fdad0
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2024-10-30 17:02:37 +0700

    fix listen mode

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 	if err != nil {
 		log.Panic("PORT must be an integer", err)
 	}
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
